dc7123700cd3d53ff151037d174dd61877fdbfc3
Joaquim Grino Llados <jgrinoll@uoc.edu>
2025-04-04 01:29:58 +0200

    Pàgina de reserves implementada.

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,16 @@
-console.log("Hello world!");
+import { Datepicker } from 'vanillajs-datepicker';
+
+const messageDisplay = document.getElementById("reservation-message")
+
+const datepickerElement = document.querySelector('input[name="bookingTimestamp"]');
+const datepicker = new Datepicker(datepickerElement, {});
+console.log(datepicker);
+
+document.getElementById("reservation-form").addEventListener("submit", function (event) {
+    event.preventDefault();
+    messageDisplay.innerText = "Requesting time slot, please wait..."
+    setTimeout(() => {
+        messageDisplay.innerText = "Your reservation has been made!"
+    }, 2000)
+    return false
+})
